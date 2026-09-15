8a133326d70254d16e98145925f80a6fe6c7d1d3
Vsilva <83793769+Vsilva20@users.noreply.github.com>
2026-09-15 12:56:22 -0300

    remove astro

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
-export default defineConfig({
-  site: 'https://vsilvatools.com.br',
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  trailingSlash: 'never',
-});
